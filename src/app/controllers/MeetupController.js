638843fcec3e40ee04c51e4dabb6524e3e4546a2
Fernando Zuffo <fzuffo@gmail.com>
2019-09-02 03:13:30 -0400

    new features

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
@@ -24,6 +24,13 @@ class MeetupController {
 
     const { title, description, location, date, banner_id } = req.body;
 
+    if (isBefore(parseISO(date), new Date())) {
+      console.log(parseISO(date), new Date());
+      return res
+        .status(400)
+        .json({ error: "You can't create a Meetup before today. " });
+    }
+
     const meetup = await Meetup.create({
       title,
       description,
@@ -32,6 +39,52 @@ class MeetupController {
       user_id: req.userId,
       banner_id,
     });
+    return res.json(meetup);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Validation fails, field required or date is incorrect.',
+      });
+    }
+
+    const { title, description, location, date, banner_id } = req.body;
+
+    //  Verifica qual o numero do Id do usuario com o usuario autenticado
+    const checkId = await Meetup.findAll({
+      raw: true,
+      where: { id: req.params.id },
+      attributes: ['user_id'],
+    });
+    if (checkId[0].user_id !== req.userId) {
+      return res.status(401).json({ error: 'User not autorizathed!' });
+    }
+
+    //  Verifica se a data já passou
+    if (isBefore(parseISO(date), new Date())) {
+      return res
+        .status(400)
+        .json({ error: "You can't create a Meetup before today. " });
+    }
+
+    const meetupId = await Meetup.findByPk(req.params.id);
+
+    const meetup = await meetupId.update({
+      title,
+      description,
+      location,
+      date,
+      banner_id,
+    });
+
     return res.json(meetup);
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -24,5 +24,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
 
 export default routes;
